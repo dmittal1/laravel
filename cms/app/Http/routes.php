494e7c1661650239481e4194894f067c99619f26
dmittal1 <mittald@hotmail.co.uk>
2017-12-17 18:36:55 +0000

    files dones

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Post;
-
+use App\User;
+use App\Country;
+use App\Photo;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,10 +15,10 @@ use App\Post;
 |
 */
 
-// Route::get('/', function () {
-//
-//    return view('welcome');
-// });
+Route::get('/', function () {
+
+   return view('welcome');
+});
 //
 //
 // Route::get('/about', function(){
@@ -61,12 +63,12 @@ use App\Post;
 |
 */
 
-Route::get('/insert',function(){
-
-DB::insert('insert into posts(title,content) values(?,?)', ['Laravel is awesome with Edwin','Laravel is the best thing that has happened to PHP, Period']);
-
-
-});
+// Route::get('/insert',function(){
+//
+// DB::insert('insert into posts(title,content) values(?,?)', ['Laravel is awesome with Edwin','Laravel is the best thing that has happened to PHP, Period']);
+//
+//
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -178,8 +180,176 @@ DB::insert('insert into posts(title,content) values(?,?)', ['Laravel is awesome 
 //
 // });
 
-Route::get('/create', function(){
+// Route::get('/create', function(){
+//
+//   Post::create(['title'=>'the create method', 'content'=>'WOW I\'am learning a lot of php with Edwin Diaz']);
+//
+// });
 
-  Post::create(['title'=>'the create method', 'content'=>'WOW I\'am learning a lot of php with Edwin Diaz']);
+// Route::get('/update', function(){
+//
+//   Post::where('id',2)->where('is_admin', 0)->update(['title'=>'NEW PHP TITLE','content'=>'I love my instructor Edwin']);
+//
+// });
 
-});
+// Route::get('/delete', function(){
+//
+//   $post = Post::find(2);
+//
+//   $post->delete();
+//
+// });
+
+// Route::get('/delete2', function(){
+//
+//   Post::destroy([3,4,5,6,7]);
+//
+//   // Post::where('is_admin', 0)->delete();
+//
+// });
+
+// Route::get('/softdelete',function(){
+//
+//   Post::find(2)->delete();
+//
+// });
+
+// Route::get('/readsoftdelete', function(){
+//
+//
+//
+//   // $post = Post::withTrashed()->where('is_admin',0)->get();
+//   //
+//   // return $post;
+//
+//   // $post = Post::onlyTrashed()->where('is_admin',0)->get();
+//   //
+//   // return $post;
+//
+// });
+
+// Route::get('/restore', function(){
+//
+//   Post::withTrashed()->where('is_admin',0)->restore();
+//
+// });
+
+// Route::get('/forcedelete', function(){
+//
+//   Post::onlyTrashed()->where('is_admin',0)->forceDelete();
+//
+// });
+
+
+/*
+|--------------------------------------------------------------------------
+| ELOQUENT Relationships
+|--------------------------------------------------------------------------
+|
+*/
+
+// // One to One relationship
+//
+// Route::get('/user/{id}/post/',function($id){
+//
+//   return User::find($id)->post->content;
+//
+// });
+//
+// Route::get('/post/{id}/user', function($id){
+//
+//   return Post::find($id)->user->name;
+//
+// });
+
+// One to Many relationship
+
+// Route::get('/posts', function(){
+//
+//   $user = User::find(1);
+//
+//   foreach($user->posts as $post){
+//
+//     echo $post->title . "<br>";
+//
+//   }
+//
+// });
+
+//Many to Many relationships
+
+// Route::get('/user/{id}/role', function($id){
+//
+//   $user = User::find($id)->roles()->orderBy('id','desc')->get();
+//
+//   return $user;
+
+  // foreach($user->roles as $role){
+  //
+  //   return $role->name;
+  //}
+
+//});
+
+// Accessing the intermediate table/pivot
+
+// Route::get('user/pivot', function(){
+//
+//   $user = User::find(1);
+//
+//   foreach($user->roles as $role){
+//
+//     echo $role->pivot->created_at;
+//
+//   }
+//
+// });
+//
+//
+// Route::get('/user/country', function(){
+//
+// $country = Country::find(1);
+//
+// foreach($country->posts as $post) {
+//
+//   return $post->title;
+//
+// }
+//
+// });
+
+// Polymorphic Relations
+
+  // Route::get('user/photos', function(){
+  //
+  //   $user = User::find(1);
+  //
+  //     foreach($user->photos as $photo) {
+  //
+  //         return $photo->path;
+  //
+  //     }
+  //
+  //
+  // });
+  //
+  // Route::get('post/{id}/photos', function($id){
+  //
+  //   $post = Post::find($id);
+  //
+  //     foreach($post->photos as $photo) {
+  //
+  //         echo $photo->path . "<br>";
+  //
+  //     }
+  //
+  //
+  // });
+
+  // Route::get('photo/{id}/post',function($id){
+  //
+  //   $photo = Photo::findOrFail($id);
+  //
+  //     return $photo->imageable;
+  //
+  // });
--- cms/app/Post.php
+++ cms/app/Post.php
@@ -3,14 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     //
 
+    use SoftDeletes;
+
     // protected $table = 'posts';
     //
     // protected $primarykey = 'post_id';
+
+    protected $dates = ['deleted_at'];
 
     protected $fillable = [
 
@@ -19,5 +24,17 @@ class Post extends Model
 
 
     ];
+
+    public function user(){
+
+      return $this->belongsTo('App\User');
+
+    }
+
+    public function photos(){
+
+      return $this->morphMany('App\Photo','imageable');
+
+    }
 
 }
